794f59db0e4a6bd9bc79d20f5a39edf28d65c219
BrighamM <brigham.michaelis@gmail.com>
2017-03-13 11:10:43 -0600

    All in one

--- Project people and book/book/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  
-
-  resources :bookitems
-
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
