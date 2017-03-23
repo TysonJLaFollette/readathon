e8fe37a2d91c2f99f91f89900e77e364b193ba6b
Bridget <lynnbridgegirl@gmail.com>
2017-03-22 18:37:19 -0600

    Error validation on adding new book

--- app/models/bookitem.rb
+++ app/models/bookitem.rb
@@ -1,2 +1,9 @@
 class Bookitem < ApplicationRecord
+  validates_presence_of :title, :author
+  validates_uniqueness_of :title
+  validates_numericality_of :pages,
+    :message => "Should be numeric value."
+
+  #need to check for white space characters at the end of a statement
+
 end
