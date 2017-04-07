82c17ad7be411755a0cef276ce7a71cdf64c8cdd
Tyson J. LaFollette <tysonlafollette@gmail.com>
2017-04-07 11:28:46 -0600

    Merged AddTeacherClassSummary branch.

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get "app/views/welcome/contact", to: "welcome#contact", as: "contact"
   get "app/views/welcome/careers", to: "welcome#careers", as: "careers"
   get "app/views/welcome/about", to: "welcome#about", as: "about"
+  get "app/views/welcome/classsummary", to: "welcome#classsummary", as: "classsummary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
