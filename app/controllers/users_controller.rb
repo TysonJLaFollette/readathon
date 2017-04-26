8e6abce01a2259c0f43330dafc0eb14b0b287f49
Bridget <lynnbridgegirl@gmail.com>
2017-04-25 19:46:02 -0600

    Fixed new user story

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,34 @@ class UsersController < ApplicationController
 
 		@user = User.find(params[:id])
 		@newUserBook = Users2book.new
+
+		@page_sum = 0
+		@books_total = 0
+			@booklist = Users2book.all.each do |user2book|
+				if user2book.userid === @user.id
+				 @page_sum = @page_sum + Bookitem.find_by_id(user2book.bookid).pages
+				 @books_total = @books_total + 1
+				end
+			end
+		@user.pages_read = @page_sum
+		@user.books_read = @books_total
+
+		#this will implement the users reading level based on # of pages and books read
+		@standing = ""
+		if @user.pages_read >= 0 && @user.books_read >= 0
+			@standing = "Novice"
+		end
+		if @user.pages_read > 200 && @user.books_read > 3
+			@standing = "Apprentice"
+		end
+		if @user.pages_read > 1000 && @user.books_read > 10
+			@standing = "Journey Man"
+		end
+		if @user.pages_read > 10000 && @user.books_read > 20
+			@standing = "Master"
+		end
+		@user.reading_level = @standing
+		@user.save
 	end
 
 
@@ -56,7 +84,13 @@ class UsersController < ApplicationController
 
 
 	def create
+
 		@user = User.new(user_params)
+
+		@user.pages_read = 0
+		@user.books_read = 0
+
+		@user.reading_level = "Novice"
 
 		if @user.save
 			redirect_to users_path
